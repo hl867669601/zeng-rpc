b9d9c485dc694dcd09118a72078462a10f0e7b68
zeng <zenghanlin97@163.com>
2022-11-29 19:34:03 +0800

    手写rpc

---
+++ src/main/java/club/xiaozeng/common/enums/RpcErrorEnum.java
@@ -0,0 +1,19 @@
+package club.xiaozeng.common.enums;
+
+import lombok.Data;
+
+/**
+ * @time: 2022/11/29 19:26
+ * @author: zengh
+ * @description:
+ */
+public enum  RpcErrorEnum {
+
+    CLIENT_CONNECT_FAILED("客户端失败"),
+    SERVER_ERROR("服务器内部错误");
+    private final String message;
+
+    RpcErrorEnum(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/club/xiaozeng/common/exception/RpcException.java
@@ -0,0 +1,14 @@
+package club.xiaozeng.common.exception;
+
+import lombok.Data;
+
+/**
+ * @time: 2022/11/29 19:25
+ * @author: zengh
+ * @description:
+ */
+@Data
+public class RpcException extends RuntimeException {
+
+
+}
