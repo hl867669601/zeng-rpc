0da68c7123c464233d8908c3fd004aee92b3a25a
zeng <zenghanlin97@163.com>
2022-11-29 19:59:42 +0800

    集成logback

---
+++ src/main/java/club/xiaozeng/common/M.java
@@ -0,0 +1,19 @@
+package club.xiaozeng.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @time: 2022/11/29 19:49
+ * @author: zengh
+ * @description:
+ */
+public class M {
+    private static final Logger logger = LoggerFactory.getLogger(M.class);
+
+    public static void main(String[] args) {
+        logger.info("ss");
+
+    }
+}
--- src/main/java/club/xiaozeng/common/enums/RpcErrorEnum.java
+++ src/main/java/club/xiaozeng/common/enums/RpcErrorEnum.java
@@ -1,6 +1,7 @@
 package club.xiaozeng.common.enums;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @time: 2022/11/29 19:26
@@ -12,7 +13,6 @@ public enum  RpcErrorEnum {
     CLIENT_CONNECT_FAILED("客户端失败"),
     SERVER_ERROR("服务器内部错误");
     private final String message;
-
     RpcErrorEnum(String message) {
         this.message = message;
     }
