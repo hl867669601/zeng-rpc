5c03dd5a7bb99fc554a8ee6975c13386c28b42e6
zeng <zenghanlin97@163.com>
2022-12-09 20:56:23 +0800

    压缩与序列化单元测试

---
+++ src/main/java/club/xiaozeng/common/entity/User.java
@@ -0,0 +1,9 @@
+package club.xiaozeng.common.entity;
+
+/**
+ * @time: 2022/12/9 20:24
+ * @author: zengh
+ * @description:
+ */
+public class User {
+}
---
+++ src/test/java/club/xiaozeng/test/SerializerTest.java
@@ -0,0 +1,9 @@
+package club.xiaozeng.test;
+
+/**
+ * @time: 2022/12/9 20:52
+ * @author: zengh
+ * @description:
+ */
+public class SerializerTest {
+}
